59094db8773dbeaf48b7a74e59a86b3953dbdbf5
Noise tg <137701851+kunjava123@users.noreply.github.com>
2024-11-01 23:20:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
 # bot
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '22980696'))
+API_HASH = environ.get('API_HASH', '2b653cb53821a82097efaba6732f5d75')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7776137055:AAGhSffaIGl1MvUUl1969RiOBcsFhK9zSsk')
 
 # database
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://Zfile:zfile@cluster0.erkr0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
